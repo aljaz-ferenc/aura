3dbd32b5c0c7ed7ea0df9dac6178d629aa49faf6
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2025-09-29 18:09:34 +0200

    feat: setup database and auth

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -15,23 +15,27 @@ export async function POST(req: NextRequest) {
 
     switch (eventType) {
       case "user.created": {
-        // const { email_addresses, first_name, image_url, last_name, username } =
-        //   evt.data;
-        // const user = {
-        //   email: email_addresses[0].email_address,
-        //   firstName: first_name,
-        //   clerkId: id,
-        //   image: image_url,
-        //   lastName: last_name,
-        //   username,
-        // };
+        const { email_addresses, first_name, image_url, last_name, username } =
+          evt.data;
+        const user = {
+          email: email_addresses[0].email_address,
+          firstName: first_name,
+          clerkId: id,
+          image: image_url,
+          lastName: last_name,
+          username,
+        };
+        console.log("USER CREATED: ", user);
         // TODO: Create user
         break;
       }
       case "user.deleted":
+        console.log("USER DELETED EVT");
         // TODO: Delete user
         break;
       case "user.updated":
+        console.log("USER UPDATED EVT");
+        break;
       // TODO: Update user
       default:
         console.warn(`Unhandled event type: ${eventType}`);
